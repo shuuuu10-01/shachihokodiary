5dd055ef893532fa3384022b68f429084d0f8bfa
kyuuri1101 <kyuuri11013@gmail.com>
2021-06-19 03:14:22 +0900

    kanjivg,、svg-loader、anime.jsの導入

---
+++ components/kanjivg/SVGAnimation.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="svg_field">
+    <div class="svg" v-for="svgpath in svgpaths" :key="svgpath.key">
+      <SVGElement v-show="svgpath != '' && btnFlag" :name="svgpath" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SVGElement from './SVGElement.vue';
+
+export default {
+  components: { SVGElement },
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: ""
+    }
+  },
+  data: () => ({
+    texts: [],
+    svgpaths: [],
+    btnFlag: false
+  }),
+  methods: {
+    draw() {
+      this.texts = [];
+      this.svgpaths = [];
+      this.texts = this.text.split("");
+      this.texts.forEach(e => {
+        this.svgpaths.push(("000" + e.charCodeAt(0).toString(16)).slice(-5));
+      });
+      console.log("svgpaths:[" + this.svgpaths + "]");
+      this.btnFlag = true;
+    },
+    reset() {
+      this.texts = [];
+      this.svgpaths = [];
+      this.btnFlag = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.svg_field {
+  display: flex;
+  width: 600px;
+  height: 500px;
+  flex-wrap: wrap;
+  align-content: flex-start;
+}
+</style>
---
+++ components/kanjivg/SVGElement.vue
@@ -0,0 +1,40 @@
+<template>
+  <component :is="svg" />
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: "0002c"
+    }
+  },
+  computed: {
+    svg() {
+      return () =>
+        import(`@/node_modules/@madcat/kanjivg/dist/main/${this.name}.svg`);
+    }
+  },
+  updated: function() {
+    this.$anime({
+      targets: "path",
+      strokeDashoffset: [this.$anime.setDashoffset, 0],
+      easing: "easeInOutSine",
+      duration: 500,
+      delay: function(el, i) {
+        return i * 250;
+      }
+    });
+  }
+};
+</script>
+
+<style scoped>
+svg {
+  width: 40px;
+  height: 40px;
+  border-bottom: solid 1px;
+}
+</style>
