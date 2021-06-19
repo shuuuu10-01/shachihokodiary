c62f7484821e98aebf6eb30f1e5e1b00b7a8ffbc
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2021-06-19 22:46:11 +0900

    [update] 画像の添付の実装

--- components/book/RightPage.vue
+++ components/book/RightPage.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="page">
-    <img id="main" :src="image" alt="diary">
+    <label v-if="edit" for="pick-img">
+      <input
+        id="pick-img"
+        ref="preview"
+        type="file"
+        accept="image/*"
+        :multiple="false"
+        style="display: none"
+        @change="onFilePicked($event)"
+      >
+      <img id="main" :src="image_model" alt="diary">
+    </label>
+    <img id="main" :src="image_model" alt="diary" v-if="!edit">
     <img id="right" src="@/assets/img/tape_right.png" alt="tape">
     <img id="left" src="@/assets/img/tape_left.png" alt="tape" v-if="!edit">
   </div>
@@ -8,6 +20,13 @@
 
 <script>
 export default {
+  data() {
+    return {
+      file: {},
+      fileName: '',
+      image_model: ''
+    }
+  },
   props: {
     image: {
       type: String,
@@ -16,6 +35,26 @@ export default {
     edit: {
       type: Boolean,
       default: false
+    }
+  },
+  watch: {
+    image() {
+      this.image_model = this.image
+    }
+  },
+  created() {
+    this.image_model = this.image
+  },
+  methods: {
+    onFilePicked (e) {
+      const file = (e.target.files || e.dataTransfer.files)
+      if (file.length !== 0) {
+        this.image_model = URL.createObjectURL(file[0])
+        this.file = file[0]
+        this.fileName = file[0].name
+      }
+      const refs = this.$refs.preview
+      refs.value = ''
     }
   }
 }
@@ -30,12 +69,13 @@ export default {
   background-image: url(https://www.beiz.jp/images_P/paper/paper_00108.jpg);
   box-shadow: inset rgb(0 0 0 / 30%) 10px 0px 14px 7px !important
 }
-.page img {
+.page img, label img {
   height: 300px;
   object-fit: cover;
 }
-.page img#main {
+.page img#main, label img#main {
   box-shadow: rgb(0 0 0 / 30%) -2px 2px 5px 1px !important;
+  width: 300px;
 }
 .page img#left {
   position: absolute;
