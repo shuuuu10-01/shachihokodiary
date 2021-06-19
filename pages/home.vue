154fdbb865301220d60ab3e8dda48118e733d10f
catgolem <kugi1255@icloud.com>
2021-06-19 19:37:14 +0900

    {fix}Removal of useless display

--- pages/home.vue
+++ pages/home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    {{ get_user }}
-    {{ get_nowlogin }}
     <turn-book />
   </div>
 </template>
@@ -10,13 +8,5 @@
 import TurnBook from "../components/book/TurnBook.vue";
 export default {
   components: { TurnBook },
-  computed: {
-    get_user() {
-      return this.$store.getters["auth/getUser"];
-    },
-    get_nowlogin() {
-      return this.$store.getters["auth/isAutheticated"];
-    }
-  }
 };
 </script>
