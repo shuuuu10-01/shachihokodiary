b41d24c4bbe7472f7a0b3d03fceb2345ef0a2701
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-06-19 18:48:11 +0900

    [add] 表紙と裏表紙の追加

---
+++ components/book/BackCover.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="back-cover">
+      <img src="../../assets/img/back-himo.png" id="back_himo"/>
+      <img src="../../assets/img/back_cover_page.png" id="back_cover_page"/>
+  </div>
+</template>
+
+
+<style>
+.back-cover {
+    position: relative;
+    border-top-left-radius: 0px;
+    border-top-right-radius: 50px;
+    border-bottom-right-radius: 50px;
+    border-bottom-left-radius: 0px;
+}
+.back-cover img {
+    position: absolute;
+}
+#back_cover_page {
+    height: 539px;
+    left: -2px;
+}
+#back_himo {
+    height: 555px;
+    right: 0;
+    top: -6px;
+}
+</style>
