c2dd28173e47c66f6b95b95589118379516de163
kyuuri1101 <kyuuri11013@gmail.com>
2021-06-19 13:33:28 +0900

    [update] 手動マージ

--- pages/home.vue
+++ pages/home.vue
@@ -2,18 +2,21 @@
   <div>
     {{ get_user }}
     {{ get_nowlogin }}
+    <turn-book />
   </div>
 </template>
 
 <script>
+import TurnBook from "../components/book/TurnBook.vue";
 export default {
+  components: { TurnBook },
   computed: {
     get_user() {
-      return this.$store.getters['auth/getUser']
+      return this.$store.getters["auth/getUser"];
     },
     get_nowlogin() {
-      return this.$store.getters['auth/isAutheticated']
+      return this.$store.getters["auth/isAutheticated"];
     }
   }
-}
+};
 </script>
