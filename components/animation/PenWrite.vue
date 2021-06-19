b43050d5a1d73bd978b64bdd79b424d3e3a5027b
catgolem <kugi1255@icloud.com>
2021-06-20 08:47:28 +0900

    途中経過

--- components/animation/PenWrite.vue
+++ components/animation/PenWrite.vue
@@ -1,62 +1,73 @@
 <template>
   <div class="animation_field">
-    <img id="pen" src="@/assets/img/pen_write.png" style="transform: translate(500px, 150px);">
+    <img id="pen" src="@/assets/img/pen_write.png" >
     <!-- <img id="shadow" src="@/assets/img/pen_shadow.png" > -->
   </div>
 </template>
 
 <script>
 
-const target = document.getElementById('pen'); // アニメーションの対象
-const startX = 50; // X座標の始点
-const startY = 50; // Y座標の始点
-const endX = 800; // X座標の目標点
-const endY = 150; // Y座標の目標点
-const diffX = endX - startX; // X座標の変化量
-const diffY = endY - startY; // Y座標の変化量
-const time = 1000; // 変化にかける時間 (ミリ秒)
-let progress = 0;
+// const target = document.getElementById('pen'); // アニメーションの対象
+// const startX = 50; // X座標の始点
+// const startY = 50; // Y座標の始点
+// const endX = 800; // X座標の目標点
+// const endY = 150; // Y座標の目標点
+// const diffX = endX - startX; // X座標の変化量
+// const diffY = endY - startY; // Y座標の変化量
+// const time = 1000; // 変化にかける時間 (ミリ秒)
+// let progress = 0;
 
-// アップデートを実行する
-requestAnimationFrame(update);
+// // アップデートを実行する
+// requestAnimationFrame(update);
 
-function update(timestamp) {
-  // 進捗率を算出
-  progress = timestamp / time;
+// function update(timestamp) {
+//   // 進捗率を算出
+//   progress = timestamp / time;
 
-  // 進捗率が100%を超えないよう丸める
-  progress = Math.min(progress, 1);
+//   // 進捗率が100%を超えないよう丸める
+//   progress = Math.min(progress, 1);
 
-  // 値を算出し反映する
-  const resultX = startX + diffX * progress; // X座標
-  const resultY = startY + diffY * progress; // Y座標
-  target.style.transform = `translate( ${resultX}px, ${resultY}px )`;
+//   // 値を算出し反映する
+//   const resultX = startX + diffX * progress; // X座標
+//   const resultY = startY + diffY * progress; // Y座標
+//   target.style.transform = `translate( ${resultX}px, ${resultY}px )`;
 
-  // 進捗が1未満の場合はアップデートを再実行する
-  if (progress < 1) {
-    requestAnimationFrame(update);
-  }
-}
+//   // 進捗が1未満の場合はアップデートを再実行する
+//   if (progress < 1) {
+//     requestAnimationFrame(update);
+//   }
+// }
   
 </script>
 
 <style scoped>
-img {
-  transform: translate(20%, 20%);
+.animation_field {
+  position:absolute;
+  top: 10px;
+  left:0;
+  right:0;
+  width: 385px;
+  height: 430px;
+  padding: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  z-index: 10000;
 }
 
 #pen {
-  position:absolute; top:100px; left:100px;
+  position:relative; top:100px; left:20px;
   width: 15%;
   height: 15%;
   display: flex;
+  z-index: 10000;
 }
 
 #shadow{
-  position:absolute; top:50px; left:50px;
+  position:relative; top:50px; left:50px;
   width: 20%;
   height: 20%;
   display: flex;
+  z-index: 10000;
 }
 
 </style>
