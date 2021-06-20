6849e141772d3784cd05f345a19150c1b689d174
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-06-20 14:48:27 +0900

    [fix] rootページのデザイン修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        ShachihokoDiary
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div v-if="getNowLogin">
+      <button type=“button” @click="$router.push('/home')">ShachihokoDiary</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    getNowLogin () {
+      return this.$store.getters['auth/isAutheticated']
+    }
+  }
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+@font-face {
+	font-family: 'MyFont1';
+	src: url(../assets/font/DancingScript-VariableFont_wght.ttf);
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
+@font-face {
+	font-family: 'MyFont2';
+	src: url(../assets/font/NewTegomin-Regular.ttf);
+}
+button {
+  font-family: MyFont1;
+  color: rgb(255, 255, 231);
   font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  margin-top: 20%;
+  background-color: rgba(255, 255, 255, 0);
+  cursor: pointer;
+  border: none;
+  outline: none;
 }
 </style>
